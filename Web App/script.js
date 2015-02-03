71c3cf5ebfee4b2492c517f70a543a0e9586a930
tsnajera <troynajera@gmail.com>
2015-02-03 00:31:51 -0800

    Added Pages
    Added Pages so that all results can now be displayed. 10 results per
    page.

--- Web App/script.js
+++ Web App/script.js
@@ -1,10 +1,17 @@
-function buttonClicked(){
-			var url = "https://api.github.com/search/repositories?q=" + 
-							document.getElementById("inputText").value + "&per_page=50";
-			ajaxGetJSON(url);
+		var resultsPerPage = 10;
+		var baseURL = "https://api.github.com/search/repositories?q=";
+		var pageNumber = 1;
+		
+		function searchButtonClicked(){
+			var url = baseURL + document.getElementById("inputText").value 
+			+ "&per_page="+ resultsPerPage;
+			document.getElementById("myForm").reset();	//reset form to clear previous select values
+			ajaxGetJSON(url, true);
+			pageNumber = 1 ;
+			
 		}
 		
-		function ajaxGetJSON(url){
+		function ajaxGetJSON(url, populateSelect){
 			// Create our XMLHttpRequest object
 			var hr = new XMLHttpRequest();
 			hr.open("GET", url, true);	
@@ -16,6 +23,12 @@ function buttonClicked(){
 				if(hr.readyState == 4 && hr.status == 200) {
 					var data = JSON.parse(hr.responseText);
 					results.innerHTML = "<h2><a href='https://github.com/search'>Search Results</a></h2>";
+					if(Boolean(populateSelect)){
+						var totalPages = Math.ceil(data.total_count/resultsPerPage);
+						if((totalPages*resultsPerPage) > 1000)	//limit
+							totalPages = 1000/resultsPerPage;
+						populatePageSelect(totalPages);
+					}
 					displayData(data);
 				}
 			}
@@ -25,6 +38,7 @@ function buttonClicked(){
 		
 		function displayData(data){
 			var i;
+			
 			for(i=0; i<data.total_count; i++){
 				//last updated
 				var updated = data.items[i]['updated_at'];
@@ -43,9 +57,32 @@ function buttonClicked(){
 					language = "<br>Written in " + language;
 					
 				//populate div
-				results.innerHTML += "<article class='topContent'><header><h2>"+(i+1)+") <a href='" + 
+				results.innerHTML += "<article class='topContent'><header><h2>"+getItemNumber(i+1)+") <a href='" + 
 				data.items[i]["html_url"]+"'>"+data.items[i]['full_name']+
 				"</a></h2></header><hr><content><p class='post-info'>"+updatedNew+description+language+
 				"</p></content></article>";
 			}
-}
+		}
+		
+		function getItemNumber(i){
+			return (((pageNumber-1)*10) +i);
+		}
+		
+		function populatePageSelect(totalPages){	//only called after a new search
+			var i;
+			for(i=1; i<=totalPages; i++){
+				optionList.innerHTML+="<option>"+i+"</option>";
+			}
+		}
+		
+		function handleSelect(selectForm){
+			var selectIndex = selectForm.optionList.selectedIndex;	//find which index is selected
+			pageNumber = selectForm.optionList.options[selectIndex].text;	//find what the text of selected index is
+		
+			var url = baseURL + document.getElementById("inputText").value 
+			+"&per_page="+ resultsPerPage+ "&page=" + pageNumber;
+			ajaxGetJSON(url, false);
+		}
+		
+		
+		
